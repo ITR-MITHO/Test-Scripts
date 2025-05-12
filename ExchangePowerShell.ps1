beb43db176eecd48e88ccd3d5f9f5a39d448682c
MITHO <123701392+ITR-MITHO@users.noreply.github.com>
2025-05-12 09:25:51 +0200

    Update ExchangePowerShell.ps1

--- ExchangePowerShell.ps1
+++ ExchangePowerShell.ps1
@@ -7,5 +7,5 @@ mkdir "C:\ITM8 - Scripts" | Out-Null
 $PS = Invoke-RestMethod -Uri "https://raw.githubusercontent.com/ITR-MITHO/Test-Scripts/refs/heads/main/PowerShell"
 $Bat = Invoke-RestMethod -Uri "https://raw.githubusercontent.com/ITR-MITHO/Test-Scripts/refs/heads/main/Bat"
 
-$PS | Out-File -FilePath "$Home\Desktop\PS_Template.ps1" -Encoding UTF8
-$BAT | Out-File -FilePath "$Home\Desktop\PS_Template.BAT" -Encoding ASCII
+$PS | Out-File -FilePath "C:\ITM8 - Scripts\ExchangePowerShell.ps1" -Encoding UTF8
+$BAT | Out-File -FilePath "C:\ITM8 - Scripts\ExchangePowerShell.BAT" -Encoding ASCII
